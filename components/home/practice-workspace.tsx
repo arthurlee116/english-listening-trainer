5e8fd90c56d80fedf6e8968f241adec607f50224
arthurli <laoli3699@qq.com>
2026-02-01 16:38:46 +0800

    fix: remove isAuthenticated prop from ResultsDisplay usage to fix type error

--- components/home/practice-workspace.tsx
+++ components/home/practice-workspace.tsx
@@ -103,7 +103,6 @@ export function PracticeWorkspace({
           exercise={currentExercise}
           onRestart={onRestart}
           onExport={onExport}
-          isAuthenticated={isAuthenticated}
         />
       </div>
     )
